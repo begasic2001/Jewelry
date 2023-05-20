a434a5f1c3c02375a230fd7507e6c885cabfc8b9
minh thanh <luongminhthanh91@gmail.com>
2023-05-20 17:40:46 +0700

    fix some bug

--- lib/Screens/history/history_srceen.dart
+++ lib/Screens/history/history_srceen.dart
@@ -11,21 +11,12 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class HistoryPage extends StatefulWidget {
-  const HistoryPage({super.key});
-
-  @override
-  State<HistoryPage> createState() => _HistoryPageState();
+  _HistoryPageState createState() => _HistoryPageState();
 }
 
 class _HistoryPageState extends State<HistoryPage> {
-  List<OrderModel> historyDatas = new List<OrderModel>.empty(growable: true);
-  final user = FirebaseAuth.instance.currentUser!;
   AppBar buildAppbar() {
     return AppBar(
-      title: Text("Lịch Sử",
-          style: TextStyle(
-            color: AppColors.baseBlackColor,
-          )),
       elevation: 0,
       backgroundColor: Colors.transparent,
       leading: IconButton(
@@ -35,80 +26,162 @@ class _HistoryPageState extends State<HistoryPage> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    HistoryUser();
-    return Scaffold(
-      appBar: buildAppbar(),
-      backgroundColor: AppColors.baseGrey10Color,
-      body: ListView(
-        physics: BouncingScrollPhysics(),
-        children: [
-          GridView.builder(
-            shrinkWrap: true,
-            primary: true,
-            itemCount: historyDatas.length,
-            physics: NeverScrollableScrollPhysics(),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 1,
-              childAspectRatio: 0.7,
+  Widget buildConfirmationProduct() {
+    return Card(
+      child: Container(
+        height: 140,
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://kickz.akamaized.net/en/media/images/p/600/adidas_originals-3_STRIPES_T_Shirt-white_-2.jpg"),
+                            )),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: const [
+                              Text(
+                                "3 Nhẫn ",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: AppColors.baseBlackColor,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "150000 VNĐ",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: AppColors.baseBlackColor,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: const [
+                              Text(
+                                "Nguyên Khối",
+                                style: TextStyle(
+                                  color: AppColors.baseDarkPinkColor,
+                                ),
+                              ),
+                              Text(
+                                "100000 VNĐ",
+                                style: TextStyle(
+                                  decoration: TextDecoration.lineThrough,
+                                  fontSize: 16,
+                                  color: AppColors.baseBlackColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            "Màu Sắc:\tVàng",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: AppColors.baseGrey60Color,
+                            ),
+                          ),
+                          Text(
+                            "Số Lượng:\tx1",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: AppColors.baseGrey60Color,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            itemBuilder: (BuildContext context, int index) {
-              var historyItems = historyDatas[index];
-              return HistoryProductWidget(
-                  //date_order: historyItems.date_order,
-                  productImage: historyItems.productImage,
-                  productName: historyItems.productName,
-                  productPrice: historyItems.productPrice,
-                  productQuantity: historyItems.productPrice,
-                  totalPrice: historyItems.totalPrice);
-            },
-          ),
-          // Padding(
-          //   padding: EdgeInsets.symmetric(
-          //     horizontal: 12.0,
-          //   ),
-          //child:
-          //),
-        ],
+          ],
+        ),
       ),
     );
   }
 
-  void HistoryUser() {
-    DatabaseReference ref =
-        FirebaseDatabase.instance.ref().child('Order').child(user.uid);
-    ref.onValue.listen((DatabaseEvent event) {
-      final map = event.snapshot.value as Map<dynamic, dynamic>;
-      historyDatas.clear();
-      map.forEach((key, value) {
-        ref.child(key).onValue.listen((event2) {
-          setState(() {
-            final map2 = event2.snapshot.value as Map<dynamic, dynamic>;
-            map2.forEach((key, value2) {
-              var colothSingleProductModel =
-                  new OrderModel.fromJson(json.decode(json.encode(value2)));
-              colothSingleProductModel.key = key;
-              historyDatas.add(colothSingleProductModel);
-            });
-          });
-        });
-      });
-    });
-
-    //  dbRef.onValue.listen((event) {
-    //   setState(() {
-    //     final map = event.snapshot.value as Map<dynamic, dynamic>;
-    //     arrivalsDatas.clear();
-
-    //     map.forEach((key, value) {
-    //       var arrivalSingleProductModel =
-    //           SingleProductModel.fromJson(json.decode(json.encode(value)));
-    //       arrivalSingleProductModel.key = key;
-    //       arrivalsDatas.add(arrivalSingleProductModel);
-    //     });
-    //     //arrivalsDatas
-    //   });
-    // });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppbar(),
+      backgroundColor: AppColors.baseGrey10Color,
+      body: ListView(
+        children: [
+          Container(
+            margin: EdgeInsets.only(bottom: 10.0),
+            color: AppColors.baseWhiteColor,
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text(
+                    "Có 2 đơn hàng",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: AppColors.baseBlackColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                
+                ),
+                buildConfirmationProduct(),
+                buildConfirmationProduct(),
+                buildConfirmationProduct(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
+  // void HistoryUser() {
+  //   DatabaseReference ref =
+  //       FirebaseDatabase.instance.ref().child('Order').child(user.uid);
+  //   ref.onValue.listen((DatabaseEvent event) {
+  //     final map = event.snapshot.value as Map<dynamic, dynamic>;
+  //     historyDatas.clear();
+  //     map.forEach((key, value) {
+  //       ref.child(key).onValue.listen((event2) {
+  //         setState(() {
+  //           final map2 = event2.snapshot.value as Map<dynamic, dynamic>;
+  //           map2.forEach((key, value2) {
+  //             var colothSingleProductModel =
+  //                 new OrderModel.fromJson(json.decode(json.encode(value2)));
+  //             colothSingleProductModel.key = key;
+  //             historyDatas.add(colothSingleProductModel);
+  //           });
+  //         });
+  //       });
+  //     });
+  //   });
+
+  
+  
+
